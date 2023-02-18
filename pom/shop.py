93224222c521b5bfc059345b630c09c524809a0a
Rafal Jablonski <jablonski.rafal@gmail.com>
2023-02-19 00:23:16 +0100

    test: add example parametrized tests

--- pom/shop.py
+++ pom/shop.py
@@ -12,6 +12,9 @@ class ShopPage:
         self.view_cart_button: Locator = page.get_by_role('button', name='View Cart')
         self.browse_products_button: Locator = page.get_by_role('button', name='Browse Products')
         self.empty_cart_text: Locator = page.get_by_text('There\'s nothing in your cart!')
+        self.product_cards: Locator = page.get_by_test_id('product-card')
+        self.shop_header_total_items = page.get_by_test_id('shop-header-total-items')
+        self.shop_header_total_price = page.get_by_test_id('shop-header-total-price')
 
     def navigate(self):
         self.page.goto(self.url)
@@ -21,3 +24,6 @@ class ShopPage:
 
     def browse_products(self):
         self.browse_products_button.click()
+
+    def add_product_to_cart(self, product_name: str):
+        self.product_cards.filter(has_text=product_name).get_by_role('button').click()
--- tests/test_shop.py
+++ tests/test_shop.py
@@ -1,3 +1,5 @@
+import pytest
+
 from playwright.sync_api import expect
 
 
@@ -10,3 +12,24 @@ def test_open_empty_cart(shop_page):
 
     # Then
     expect(shop_page.empty_cart_text).to_be_visible()
+
+
+@pytest.mark.parametrize(
+    'product_name, total_items, total_price',
+    [
+        ('Watch', 1, '$99.99'),
+        ('iPhone 6', 1, '$499.99'),
+        ('MacBook', 1, '$999.99')
+    ]
+)
+def test_add_product_to_cart(shop_page, product_name, total_items, total_price):
+    # Given
+    expect(shop_page.shop_header_total_items).to_have_text('Total Items: 0')
+    expect(shop_page.shop_header_total_price).to_have_text('Total Price: $0.00')
+
+    # When
+    shop_page.add_product_to_cart(product_name)
+
+    # Then
+    expect(shop_page.shop_header_total_items).to_have_text(f'Total Items: {total_items}')
+    expect(shop_page.shop_header_total_price).to_have_text(f'Total Price: {total_price}')
