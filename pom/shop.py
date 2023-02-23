3068a3b5ff8cd69e06bc781ca255f826099f135a
Rafal Jablonski <jablonski.rafal@gmail.com>
2023-02-23 22:42:19 +0100

    test: remove product item from cart; e2e place order

--- pom/shop.py
+++ pom/shop.py
@@ -13,9 +13,11 @@ class ShopPage:
         self.browse_products_button: Locator = page.get_by_role('button', name='Browse Products')
         self.empty_cart_text: Locator = page.get_by_text('There\'s nothing in your cart!')
         self.product_cards: Locator = page.get_by_test_id('product-card')
-        self.shop_header_total_items = page.get_by_test_id('shop-header-total-items')
-        self.shop_header_total_price = page.get_by_test_id('shop-header-total-price')
-        self.cart_products = page.get_by_role('listitem')
+        self.shop_header_total_items: Locator = page.get_by_test_id('shop-header-total-items')
+        self.shop_header_total_price: Locator = page.get_by_test_id('shop-header-total-price')
+        self.cart_products: Locator = page.get_by_role('listitem')
+        self.place_order_button: Locator = page.get_by_role('button', name='Place Order')
+        self.after_order_text: Locator = page.get_by_text('Thank you for your order.')
 
     def navigate(self):
         self.page.goto(self.url)
@@ -43,10 +45,19 @@ class ShopPage:
 
     def change_quantity_of_product(self, product_name: str, quantity: int):
         self.product_cart_item_quantity_dropdown(product_name).click()
-        self.page.get_by_role('option', name=str(quantity)).click()
+        self.page.get_by_role('option', name=str(quantity)).first.click()
 
     def product_cart_item_badge(self, product_name: str) -> Locator:
         return self.product_cart_item(product_name).locator('span.MuiBadge-badge')
 
     def product_cart_item_subtotal_price(self, product_name: str) -> Locator:
         return self.product_cart_item(product_name).get_by_text('Subtotal Price')
+
+    def product_cart_item_remove_button(self, product_name: str) -> Locator:
+        return self.product_cart_item(product_name).get_by_role('button', name='Remove Item From Cart')
+
+    def remove_product_from_cart(self, product_name: str):
+        self.product_cart_item_remove_button(product_name).click()
+
+    def place_order(self):
+        self.place_order_button.click()
--- tests/test_shop.py
+++ tests/test_shop.py
@@ -12,6 +12,7 @@ def test_open_empty_cart(shop_page):
 
     # Then
     expect(shop_page.empty_cart_text).to_be_visible()
+    expect(shop_page.place_order_button).to_be_disabled()
 
 
 @pytest.mark.parametrize(
@@ -53,7 +54,7 @@ def test_add_multiple_products_to_cart(shop_page):
     expect(shop_page.shop_header_total_price).to_have_text('Total Price: $1,699.96')
 
 
-def test_change_number_of_items_in_cart(shop_page):
+def test_change_number_of_product_items_in_cart(shop_page):
     # Given
     product_name = 'Watch'
 
@@ -75,3 +76,73 @@ def test_change_number_of_items_in_cart(shop_page):
     expect(shop_page.product_cart_item_badge(product_name)).to_have_text('10')
     expect(shop_page.product_cart_item_quantity_dropdown(product_name)).to_have_text('10')
     expect(shop_page.product_cart_item_subtotal_price(product_name)).to_have_text('Subtotal Price: $999.90')
+
+
+def test_remove_product_item_from_cart(shop_page):
+    # Given
+    product_name = 'Watch'
+
+    shop_page.add_product_to_cart(product_name)
+    shop_page.view_cart()
+
+    expect(shop_page.product_cart_item(product_name)).to_be_visible()
+    expect(shop_page.empty_cart_text).not_to_be_visible()
+    expect(shop_page.place_order_button).to_be_enabled()
+
+    # When
+    shop_page.remove_product_from_cart(product_name)
+
+    # Then
+    expect(shop_page.product_cart_item(product_name)).not_to_be_visible()
+    expect(shop_page.empty_cart_text).to_be_visible()
+    expect(shop_page.place_order_button).to_be_disabled()
+
+
+def test_end_to_end_place_order(shop_page):
+    # Given
+    products_to_order = [
+        {
+            'product_name': 'Watch',
+            'quantity': 3,  # 0 <= quantity
+            'quantity_by_dropdown': False
+        },
+        {
+            'product_name': 'iPhone 6',
+            'quantity': 5,
+            'quantity_by_dropdown': True
+        },
+        {
+            'product_name': 'MacBook',
+            'quantity': 1,
+            'quantity_by_dropdown': None
+        }
+    ]
+    total_quantity = 9
+    total_price = '$3,799.91'
+
+    # When
+    for product in products_to_order:
+        shop_page.add_product_to_cart(product['product_name'])
+
+        if product['quantity_by_dropdown'] or product['quantity_by_dropdown'] is None:
+            shop_page.view_cart()
+            if product['quantity'] > 0:
+                shop_page.change_quantity_of_product(product['product_name'], product['quantity'])
+            else:
+                shop_page.remove_product_from_cart(product['product_name'])
+            shop_page.browse_products()
+        else:
+            for i in range(product['quantity']-1):
+                shop_page.add_product_to_cart(product['product_name'])
+
+    shop_page.view_cart()
+
+    expect(shop_page.shop_header_total_items).to_have_text(f'Total Items: {total_quantity}')
+    expect(shop_page.shop_header_total_price).to_have_text(f'Total Price: {total_price}')
+
+    shop_page.place_order()
+
+    # Then
+    expect(shop_page.shop_header_total_items).to_have_text('Total Items: 0')
+    expect(shop_page.shop_header_total_price).to_have_text('Total Price: $0.00')
+    expect(shop_page.after_order_text).to_be_visible()
