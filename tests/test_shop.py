1ca176f36fd817ee1e1bb40f836f7cb493eb5806
Rafal Jablonski <jablonski.rafal@gmail.com>
2023-03-03 21:54:30 +0100

    test: update shop tests for new products data

--- tests/test_shop.py
+++ tests/test_shop.py
@@ -18,9 +18,9 @@ def test_open_empty_cart(shop_page):
 @pytest.mark.parametrize(
     'product_name, total_items, total_price',
     [
-        ('Watch', 1, '$99.99'),
-        ('iPhone 6', 1, '$499.99'),
-        ('MacBook', 1, '$999.99')
+        ('iPhone 9', 1, '$549.00'),
+        ('iPhone X', 1, '$899.00'),
+        ('Samsung Universe 9', 1, '$1,249.00')
     ]
 )
 def test_add_product_to_cart(shop_page, product_name, total_items, total_price):
@@ -44,43 +44,43 @@ def test_add_multiple_products_to_cart(shop_page):
     expect(shop_page.shop_header_total_price).to_have_text('Total Price: $0.00')
 
     # When
-    shop_page.add_product_to_cart('Watch')
-    shop_page.add_product_to_cart('iPhone 6')
-    shop_page.add_product_to_cart('MacBook')
-    shop_page.add_product_to_cart('Watch')
+    shop_page.add_product_to_cart('iPhone 9')
+    shop_page.add_product_to_cart('iPhone X')
+    shop_page.add_product_to_cart('Samsung Universe 9')
+    shop_page.add_product_to_cart('iPhone 9')
 
     # Then
     expect(shop_page.shop_header_total_items).to_have_text('Total Items: 4')
-    expect(shop_page.shop_header_total_price).to_have_text('Total Price: $1,699.96')
+    expect(shop_page.shop_header_total_price).to_have_text('Total Price: $3,246.00')
 
 
 def test_change_number_of_product_items_in_cart(shop_page):
     # Given
-    product_name = 'Watch'
+    product_name = 'iPhone 9'
 
     shop_page.add_product_to_cart(product_name)
     shop_page.view_cart()
 
     expect(shop_page.shop_header_total_items).to_have_text('Total Items: 1')
-    expect(shop_page.shop_header_total_price).to_have_text('Total Price: $99.99')
+    expect(shop_page.shop_header_total_price).to_have_text('Total Price: $549.00')
     expect(shop_page.product_cart_item_badge(product_name)).to_have_text('1')
     expect(shop_page.product_cart_item_quantity_dropdown(product_name)).to_have_text('1')
-    expect(shop_page.product_cart_item_subtotal_price(product_name)).to_have_text('Subtotal Price: $99.99')
+    expect(shop_page.product_cart_item_subtotal_price(product_name)).to_have_text('Subtotal Price: $549.00')
 
     # When
     shop_page.change_quantity_of_product(product_name, 10)
 
     # Then
     expect(shop_page.shop_header_total_items).to_have_text('Total Items: 10')
-    expect(shop_page.shop_header_total_price).to_have_text('Total Price: $999.90')
+    expect(shop_page.shop_header_total_price).to_have_text('Total Price: $5,490.00')
     expect(shop_page.product_cart_item_badge(product_name)).to_have_text('10')
     expect(shop_page.product_cart_item_quantity_dropdown(product_name)).to_have_text('10')
-    expect(shop_page.product_cart_item_subtotal_price(product_name)).to_have_text('Subtotal Price: $999.90')
+    expect(shop_page.product_cart_item_subtotal_price(product_name)).to_have_text('Subtotal Price: $5,490.00')
 
 
 def test_remove_product_item_from_cart(shop_page):
     # Given
-    product_name = 'Watch'
+    product_name = 'iPhone 9'
 
     shop_page.add_product_to_cart(product_name)
     shop_page.view_cart()
@@ -102,23 +102,23 @@ def test_end_to_end_place_order(shop_page):
     # Given
     products_to_order = [
         {
-            'product_name': 'Watch',
+            'product_name': 'iPhone 9',
             'quantity': 3,  # 0 <= quantity
             'quantity_by_dropdown': False
         },
         {
-            'product_name': 'iPhone 6',
+            'product_name': 'iPhone X',
             'quantity': 5,
             'quantity_by_dropdown': True
         },
         {
-            'product_name': 'MacBook',
+            'product_name': 'Samsung Universe 9',
             'quantity': 1,
             'quantity_by_dropdown': None
         }
     ]
     total_quantity = 9
-    total_price = '$3,799.91'
+    total_price = '$7,391.00'
 
     # When
     for product in products_to_order:
