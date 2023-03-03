3e87f75bd0114053fc715d455986657267aedb60
Rafal Jablonski <jablonski.rafal@gmail.com>
2023-03-04 00:03:06 +0100

    test: add tests for todos page

--- pom/todos.py
+++ pom/todos.py
@@ -13,6 +13,7 @@ class TodosPage:
         self.add_task_button: Locator = page.get_by_role('button', name='ADD TASK')
         self.clear_tasks_button: Locator = page.get_by_role('button', name='CLEAR COMPLETED TASKS')
         self.todo_items: Locator = page.get_by_test_id('todo-item')
+        self.no_todos_paragraph: Locator = page.get_by_role('paragraph').filter(has_text='There are no TODOs!')
 
     def navigate(self):
         self.page.goto(self.url)
@@ -20,3 +21,19 @@ class TodosPage:
     def add_task(self, task_name: str):
         self.new_task_input_field.type(task_name)
         self.add_task_button.click()
+
+    def todo_item(self, todo_name: str) -> Locator:
+        return self.todo_items.filter(has_text=todo_name)
+
+    def todo_item_checkbox(self, todo_name: str) -> Locator:
+        return self.todo_item(todo_name).get_by_role('checkbox')
+
+    def toggle_todo_item(self, todo_name: str):
+        todo_item_checkbox = self.todo_item_checkbox(todo_name)
+        if todo_item_checkbox.is_checked():
+            todo_item_checkbox.uncheck()
+        else:
+            todo_item_checkbox.check()
+
+    def clear_completed_tasks(self):
+        self.clear_tasks_button.click()
--- tests/test_todos.py
+++ tests/test_todos.py
@@ -10,6 +10,7 @@ def test_add_todos(todos_page_authenticated):
         'Go for a walk',
         'Wash the dishes'
     ]
+    expect(todos_page.no_todos_paragraph).to_be_visible()
     expect(todos_page.todo_items).not_to_be_visible()
 
     # When
@@ -22,7 +23,7 @@ def test_add_todos(todos_page_authenticated):
         assert todo_text in tasks
 
 
-def test_check_todos_from_state(todos_page_with_state):
+def test_load_todos_from_state(todos_page_with_state):
     # Given
     todos_page = todos_page_with_state
 
@@ -39,3 +40,107 @@ def test_check_todos_from_state(todos_page_with_state):
     assert todos_page.todo_items.count() == 3
     for todo_text in todos_page.todo_items.all_text_contents():
         assert todo_text in tasks
+
+
+def test_toggle_todos(todos_page_with_state):
+    # Given
+    todos_page = todos_page_with_state
+
+    tasks = [
+        'Buy milk',
+        'Go for a walk',
+        'Wash the dishes'
+    ]
+    expect(todos_page.todo_item_checkbox(tasks[0])).not_to_be_checked()
+
+    # When
+    todos_page.toggle_todo_item(tasks[0])
+
+    # Then
+    expect(todos_page.todo_item_checkbox(tasks[0])).to_be_checked()
+
+    # When
+    todos_page.toggle_todo_item(tasks[0])
+
+    # Then
+    expect(todos_page.todo_item_checkbox(tasks[0])).not_to_be_checked()
+
+
+def test_clear_todos(todos_page_with_state):
+    # Given
+    todos_page = todos_page_with_state
+
+    tasks = [
+        'Buy milk',
+        'Go for a walk',
+        'Wash the dishes'
+    ]
+    expect(todos_page.todo_item(tasks[0])).to_be_visible()
+    expect(todos_page.todo_item(tasks[1])).to_be_visible()
+    expect(todos_page.todo_item(tasks[2])).to_be_visible()
+
+    # When
+    todos_page.toggle_todo_item(tasks[0])
+    todos_page.clear_completed_tasks()
+
+    # Then
+    expect(todos_page.todo_item(tasks[0])).not_to_be_visible()
+    expect(todos_page.todo_item(tasks[1])).to_be_visible()
+    expect(todos_page.todo_item(tasks[2])).to_be_visible()
+
+    # When
+    todos_page.toggle_todo_item(tasks[1])
+    todos_page.clear_completed_tasks()
+
+    # Then
+    expect(todos_page.todo_item(tasks[0])).not_to_be_visible()
+    expect(todos_page.todo_item(tasks[1])).not_to_be_visible()
+    expect(todos_page.todo_item(tasks[2])).to_be_visible()
+
+
+def test_todos(todos_page):
+    # Given
+    tasks = [
+        'Buy milk',
+        'Go for a walk',
+        'Wash the dishes'
+    ]
+    expect(todos_page.no_todos_paragraph).to_be_visible()
+
+    # When
+    todos_page.add_task(tasks[0])
+    todos_page.add_task(tasks[1])
+
+    # Then
+    expect(todos_page.todo_item(tasks[0])).to_be_visible()
+    expect(todos_page.todo_item(tasks[1])).to_be_visible()
+    expect(todos_page.no_todos_paragraph).not_to_be_visible()
+
+    # When
+    todos_page.toggle_todo_item(tasks[1])
+    todos_page.clear_completed_tasks()
+
+    # Then
+    expect(todos_page.todo_item(tasks[0])).to_be_visible()
+    expect(todos_page.todo_item(tasks[1])).not_to_be_visible()
+    expect(todos_page.no_todos_paragraph).not_to_be_visible()
+
+    # When
+    todos_page.add_task(tasks[2])
+
+    # Then
+    expect(todos_page.todo_item(tasks[0])).to_be_visible()
+    expect(todos_page.todo_item(tasks[1])).not_to_be_visible()
+    expect(todos_page.todo_item(tasks[2])).to_be_visible()
+    expect(todos_page.no_todos_paragraph).not_to_be_visible()
+
+    # When
+    todos_page.toggle_todo_item(tasks[0])
+    todos_page.toggle_todo_item(tasks[2])
+    todos_page.clear_completed_tasks()
+
+    # Then
+    expect(todos_page.todo_item(tasks[0])).not_to_be_visible()
+    expect(todos_page.todo_item(tasks[1])).not_to_be_visible()
+    expect(todos_page.todo_item(tasks[2])).not_to_be_visible()
+    expect(todos_page.no_todos_paragraph).to_be_visible()
